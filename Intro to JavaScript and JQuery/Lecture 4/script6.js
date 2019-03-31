2fdb5118db05e95dfb78fa36ebcefc5f84f50ca4
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2019-03-30 22:13:14 -0600

    adding assignments

---
+++ Intro to JavaScript and JQuery/Lecture 4/script6.js
@@ -0,0 +1,6 @@
+var imgElement = document.createElement("img");
+            
+            imgElement.setAttribute("src", "dragon.jpg");
+            imgElement.setAttribute("class", "demoClass");
+            
+            document.body.appendChild(imgElement);
